07d34dfaa6f4ec17a8ca3831e74d1544eeb1327a
xlsea <m@xlsea.cn>
2023-10-11 10:48:08 +0800

    新增json预览和编辑功能

--- src/main.js
+++ src/main.js
@@ -1,16 +1,19 @@
-import {createApp} from 'vue'
-import './style.css'
+import { createApp } from 'vue'
+// import './style.css'
 import App from './App.vue'
+import JsonViewer from 'vue3-json-viewer'
+import 'vue3-json-viewer/dist/index.css'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import * as elIcons from '@element-plus/icons-vue'
 
 const app = createApp(App)
 
+app.use(JsonViewer)
 app.use(ElementPlus)
 
 // 统一注册el图标
-for(let icon in elIcons){
-    app.component(`ElIcon${icon}`, elIcons[icon])
+for (let icon in elIcons) {
+  app.component(`ElIcon${icon}`, elIcons[icon])
 }
 app.mount('#app')
